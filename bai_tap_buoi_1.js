550d5f95e16220c52869668248e638089f6907f9
nvtoan198 <nvt19895@gmail.com>
2022-03-24 17:44:08 +0700

    Update bai_tap_buoi_1.js

--- bai_tap_buoi_1.js
+++ bai_tap_buoi_1.js
@@ -1,23 +1,3 @@
-// bài 1. Sử dụng vòng lặp, tính tổng giá trị các phần tử trong mảng.
-// Input: [0, 5, 4, 2, 8]
-// Output: 19
-
-// bài 2. Sử dụng một mảng các đối tượng person (name, age) thành tổng hợp mảng các tên người.
-// Input: const person = [
-// 	{ name: 'John', age: 24 },
-// 	{ name: Pete, age: 25 },
-// 	{ name: Mary', age: 28 }
-// ]
-
-// Output: ['John', 'Pete', 'Mary'].
-
-// bài 3. Sử dụng các mảng dữ liệu tên người để tổng hợp thành mảng tên người không trùng lặp.
-// Input:	arr1 = ['John', 'Pete', 'Mary']
-// 	arr2 = ['Mary', 'Henry', 'Harry']
-
-// Output: ['John', 'Pete', 'Mary', 'Henry']
-
-// Gợi ý: Có thể sử dụng spread trong mảng. [...arr] kết hợp với phương thức includes trong array method.
 // Bai 1:
 var arr = [0, 5, 4, 2, 8];
 var sum = 0;
@@ -25,18 +5,27 @@ var sum = 0;
 for (let i = 0; i < 5; i++ ){
     sum = sum + arr[i];
 }
-console.log('Tong mang: ', sum);
+
+console.log('Bai 1: Sum = ', sum);
+
 
 // Bai 2:
 const person = [
     { name: 'John', age: 24 },
-	{ name: 'Pete', age: 25 },
-	{ name: 'Mary', age: 28 },
-];
-let result = [];
-let i = 0;
-do {
-    i++;
-    result = result.concat(person.[i.name]);
-} while (i < 3);
-console.log('Tong mang: ', result);
+    { name: 'Pete', age: 25 },
+    { name: 'Mary', age: 28 }
+]
+
+var personNew = person.map(function (n) {
+    return n.name;
+})
+
+console.log('Bai 2:', personNew)
+
+
+// Bai 3: 
+var arr1 = ['John', 'Pete', 'Mary']
+var arr2 = ['Mary', 'Henry', 'Harry']
+var arr3 = arr1.concat(arr2);
+
+console.log('Bai 3:',[...(new Set(arr3))]);
